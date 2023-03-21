9aa53de1e2a330392985328a7cdd956ebfad55ca
DIFU-N <chukwudifunsofor@gmail.com>
2023-03-21 09:28:45 +0100

    active navLinks

--- src/hooks/NavBar.jsx
+++ src/hooks/NavBar.jsx
@@ -1,7 +1,8 @@
 import React from "react";
-import { NavLink } from "react-router-dom";
+import { NavLink, useLocation } from "react-router-dom";
 
 const NavBar = () => {
+  const location = useLocation();
   return (
     <div className="bg-[#05070f] text-[15px] px-14 w-full">
       <nav className=" text-[#7b7e8e] p-1 grid grid-cols-[10%_70%_20%]">
@@ -11,20 +12,32 @@ const NavBar = () => {
           <div className="flex justify-center w-full">  
             <div className="grid grid-cols-4">
               <NavLink to="/" className={'flex font-semibold items-center group relative justify-center'}>
-                <div className="my-2 px-2 mx-1 group-hover:text-[#f3c351] group-focus:text-[#f3c351]">Home</div>
-                <span class="absolute left-0 right-0 bottom-0 border-b-[4px] border-transparent transform scale-x-0 transition-transform duration-300 ease-in-out group-hover:border-b-3 group-hover:border-[#f3c351] group-hover:scale-100 group-focus:scale-x-100 group-focus:border-b-4 group-focus:border-[#f3c351]"></span>
+                <div className={(location.pathname === '/' ? "my-2 px-2 mx-1 text-[#f3c351]" : 
+                "my-2 px-2 mx-1 group-hover:text-[#f3c351] group-focus:text-[#f3c351]")}>Home</div>
+                <span className={(location.pathname === '/' ? 'absolute left-0 right-0 bottom-0 scale-x-100 border-b-4 border-[#f3c351]' 
+                : "absolute left-0 right-0 bottom-0 border-b-[4px] border-transparent transform scale-x-0 transition-transform duration-300 ease-in-out group-hover:border-b-3 group-hover:border-[#f3c351] group-hover:scale-100" 
+                )}></span>
               </NavLink>
               <NavLink to="/dash" className={'flex font-semibold items-center group relative justify-center'}>
-                <div className="my-2 px-2 mx-1 group-hover:text-[#f3c351] group-focus:text-[#f3c351]">Dashboard</div>
-                <span class="absolute left-0 right-0 bottom-0 border-b-[4px] border-transparent transform scale-x-0 transition-transform duration-300 ease-in-out group-hover:border-b-3 group-hover:border-[#f3c351] group-hover:scale-100 group-focus:scale-x-100 group-focus:border-b-4 group-focus:border-[#f3c351]"></span>
+                <div className={(location.pathname === '/dash' ? "my-2 px-2 mx-1 text-[#f3c351]" : 
+                "my-2 px-2 mx-1 group-hover:text-[#f3c351] group-focus:text-[#f3c351]")}>Dashboard</div>
+                <span className={(location.pathname === '/dash' ? 'absolute left-0 right-0 bottom-0 scale-x-100 border-b-4 border-[#f3c351]' 
+                : "absolute left-0 right-0 bottom-0 border-b-[4px] border-transparent transform scale-x-0 transition-transform duration-300 ease-in-out group-hover:border-b-3 group-hover:border-[#f3c351] group-hover:scale-100" 
+                )}></span>
               </NavLink>
               <NavLink to="/guedes" className={'flex font-semibold items-center group relative justify-center'}>
-                <div className="my-2 px-2 mx-1 group-hover:text-[#f3c351] group-focus:text-[#f3c351]">Guides</div>
-                <span class="absolute left-0 right-0 bottom-0 border-b-[4px] border-transparent transform scale-x-0 transition-transform duration-300 ease-in-out group-hover:border-b-3 group-hover:border-[#f3c351] group-hover:scale-100 group-focus:scale-x-100 group-focus:border-b-4 group-focus:border-[#f3c351]"></span>
+                <div className={(location.pathname === '/guedes' ? "my-2 px-2 mx-1 text-[#f3c351]" : 
+                "my-2 px-2 mx-1 group-hover:text-[#f3c351] group-focus:text-[#f3c351]")}>Guides</div>
+                <span className={(location.pathname === '/guedes' ? 'absolute left-0 right-0 bottom-0 scale-x-100 border-b-4 border-[#f3c351]' 
+                : "absolute left-0 right-0 bottom-0 border-b-[4px] border-transparent transform scale-x-0 transition-transform duration-300 ease-in-out group-hover:border-b-3 group-hover:border-[#f3c351] group-hover:scale-100" 
+                )}></span>
               </NavLink>
-              <NavLink to="/" className={'flex font-semibold items-center group relative justify-center'}>
-                <div className="my-2 px-2 mx-1 group-hover:text-[#f3c351] group-focus:text-[#f3c351]">News</div>
-                <span class="absolute left-0 right-0 bottom-0 border-b-[4px] border-transparent transform scale-x-0 transition-transform duration-300 ease-in-out group-hover:border-b-3 group-hover:border-[#f3c351] group-hover:scale-100 group-focus:scale-x-100 group-focus:border-b-4 group-focus:border-[#f3c351]"></span>
+              <NavLink to="/news" className={'flex font-semibold items-center group relative justify-center'}>
+                <div className={(location.pathname === '/news' ? "my-2 px-2 mx-1 text-[#f3c351]" : 
+                "my-2 px-2 mx-1 group-hover:text-[#f3c351] group-focus:text-[#f3c351]")}>News</div>
+                <span className={(location.pathname === '/news' ? 'absolute left-0 right-0 bottom-0 scale-x-100 border-b-4 border-[#f3c351]' 
+                : "absolute left-0 right-0 bottom-0 border-b-[4px] border-transparent transform scale-x-0 transition-transform duration-300 ease-in-out group-hover:border-b-3 group-hover:border-[#f3c351] group-hover:scale-100" 
+                )}></span>
               </NavLink>
             </div>
           </div>
