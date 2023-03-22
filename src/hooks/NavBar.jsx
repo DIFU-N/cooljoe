9ce98c94b3b57d3dbb59d7802ad0d64226502f3b
DIFU-N <chukwudifunsofor@gmail.com>
2023-03-22 10:24:14 +0100

    Introduction Component

--- src/hooks/NavBar.jsx
+++ src/hooks/NavBar.jsx
@@ -3,51 +3,55 @@ import { NavLink, useLocation } from "react-router-dom";
 
 const NavBar = () => {
   const location = useLocation();
+  const divStyle = {
+    fontFamily: 'Barlow, sans-serif'
+  }
   return (
-    <div className="bg-[#05070f] text-[15px] px-14 w-full">
-      <nav className=" text-[#7b7e8e] p-1 grid grid-cols-[10%_70%_20%]">
-          <div className="w-full flex ml-4">
-            <NavLink className={'font-serif p-1 first-letter:text-3xl text-lg text-green-900'}>Okay</NavLink>
+    <div className="bg-[#05070f] text-base px-9 w-full">
+      <style>
+        @import url('https://fonts.googleapis.com/css2?family=Barlow&display=swap');
+      </style>
+      <nav className=" text-[#7b7e8e] grid grid-cols-[10.5%_69.5%_20%] gap-x-[34px] overflow-hidden" style={divStyle}>
+          <div className="flex ml-4">
+            <NavLink className={'w-full my-1 first-letter:text-3xl first-letter:text-[#f3c351] text-xl text-green-900'}>O STRYDAa</NavLink>
           </div>
-          <div className="flex justify-center w-full">  
-            <div className="grid grid-cols-4">
-              <NavLink to="/" className={'flex font-semibold items-center group relative justify-center'}>
-                <div className={(location.pathname === '/' ? "my-2 px-2 mx-1 text-[#f3c351]" : 
-                "my-2 px-2 mx-1 group-hover:text-[#f3c351] group-focus:text-[#f3c351]")}>Home</div>
+          <div className="flex justify-center mt-[5px] text-[16px] w-full">
+            <div className="flex">
+              <NavLink to="/" className={'flex items-center font-medium group relative justify-center'}>
+                <div className={(location.pathname === '/' ? "mx-5 text-[#f3c351]" : 
+                "mx-5 group-hover:text-[#f3c351] group-focus:text-[#f3c351]")}>Home</div>
                 <span className={(location.pathname === '/' ? 'absolute left-0 right-0 bottom-0 scale-x-100 border-b-4 border-[#f3c351]' 
                 : "absolute left-0 right-0 bottom-0 border-b-[4px] border-transparent transform scale-x-0 transition-transform duration-300 ease-in-out group-hover:border-b-3 group-hover:border-[#f3c351] group-hover:scale-100" 
                 )}></span>
               </NavLink>
-              <NavLink to="/dash" className={'flex font-semibold items-center group relative justify-center'}>
-                <div className={(location.pathname === '/dash' ? "my-2 px-2 mx-1 text-[#f3c351]" : 
-                "my-2 px-2 mx-1 group-hover:text-[#f3c351] group-focus:text-[#f3c351]")}>Dashboard</div>
+              <NavLink to="/dash" className={'flex font-medium items-center group relative justify-center'}>
+                <div className={(location.pathname === '/dash' ? "mx-6 text-[#f3c351]" : 
+                "mx-6 group-hover:text-[#f3c351] group-focus:text-[#f3c351]")}>How It Works</div>
                 <span className={(location.pathname === '/dash' ? 'absolute left-0 right-0 bottom-0 scale-x-100 border-b-4 border-[#f3c351]' 
                 : "absolute left-0 right-0 bottom-0 border-b-[4px] border-transparent transform scale-x-0 transition-transform duration-300 ease-in-out group-hover:border-b-3 group-hover:border-[#f3c351] group-hover:scale-100" 
                 )}></span>
               </NavLink>
-              <NavLink to="/guedes" className={'flex font-semibold items-center group relative justify-center'}>
-                <div className={(location.pathname === '/guedes' ? "my-2 px-2 mx-1 text-[#f3c351]" : 
-                "my-2 px-2 mx-1 group-hover:text-[#f3c351] group-focus:text-[#f3c351]")}>Guides</div>
+              <NavLink to="/guedes" className={'flex font-medium items-center group relative justify-center'}>
+                <div className={(location.pathname === '/guedes' ? "mx-3 text-[#f3c351]" : 
+                "mx-3 group-hover:text-[#f3c351] group-focus:text-[#f3c351]")}>Guides</div>
                 <span className={(location.pathname === '/guedes' ? 'absolute left-0 right-0 bottom-0 scale-x-100 border-b-4 border-[#f3c351]' 
                 : "absolute left-0 right-0 bottom-0 border-b-[4px] border-transparent transform scale-x-0 transition-transform duration-300 ease-in-out group-hover:border-b-3 group-hover:border-[#f3c351] group-hover:scale-100" 
                 )}></span>
               </NavLink>
-              <NavLink to="/news" className={'flex font-semibold items-center group relative justify-center'}>
-                <div className={(location.pathname === '/news' ? "my-2 px-2 mx-1 text-[#f3c351]" : 
-                "my-2 px-2 mx-1 group-hover:text-[#f3c351] group-focus:text-[#f3c351]")}>News</div>
+              <NavLink to="/news" className={'flex font-medium items-center group relative justify-center'}>
+                <div className={(location.pathname === '/news' ? "mx-3 text-[#f3c351]" : 
+                "mx-3 group-hover:text-[#f3c351] group-focus:text-[#f3c351]")}>News</div>
                 <span className={(location.pathname === '/news' ? 'absolute left-0 right-0 bottom-0 scale-x-100 border-b-4 border-[#f3c351]' 
                 : "absolute left-0 right-0 bottom-0 border-b-[4px] border-transparent transform scale-x-0 transition-transform duration-300 ease-in-out group-hover:border-b-3 group-hover:border-[#f3c351] group-hover:scale-100" 
                 )}></span>
               </NavLink>
             </div>
           </div>
-          <div className="w-full flex justify-end items-center gap-x-2">
-            <NavLink>
-              <button className="rounded-2xl border-[1.5px] text-sm border-[#a5a7b5] hover:bg-slate-800 my-1 w-16 p-1">LOGIN</button>
-            </NavLink>
-            <NavLink>
-              <button className="rounded-2xl w-24 p-1 bg-[#d5ab48] hover:bg-[#f3c351] text-sm my-1 text-[#1b1b25]">SIGN UP</button>
-            </NavLink>
+          <div className="w-full flex items-center">
+            <div className="py-[10px] flex gap-x-1">
+              <button className="rounded-2xl py-1 border-[1.5px] text-[#a5a7b5] text-sm border-[#a5a7b5] hover:bg-slate-800 w-16">LOG IN</button>
+              <button className="rounded-2xl w-24 p-1 bg-[#d5ab48] hover:bg-[#f3c351] text-sm text-[#1b1b25]">SIGN UP</button>
+            </div>
           </div>
       </nav>
     </div>
---
+++ src/pages/HomePage/IntroComp.jsx
@@ -0,0 +1,38 @@
+import React from 'react';
+import ArrowButton from '../../hooks/ArrowButton';
+
+const IntroComp = () => {
+    const divStyle = {
+        // fontFamily: 'Yantramanav, sans-serif',
+        fontFamily: 'Bebas Neue, cursive'
+        // fontFamily: 'Six Caps, sans-serif',
+        // fontFamily: 'Oswald, sans-serif'
+    };
+   const divStyle1 = {
+        fontFamily: 'Barlow, sans-serif'
+    }
+    return (
+        <div className='grid grid-cols-2 pt-36 mx-9 overflow-hidden bg-[#05070f] text-white pb-40'>
+            <style>
+                @import url('https://fonts.googleapis.com/css2?family=Bebas+Neue&family=Oswald&family=Yantramanav&family=Six+Caps&display=swap');
+            </style>
+            <div className='grid gap-y-6 w-[80%] ml-4'>
+                <span className='text-[4.7rem] h-[6rem] font-black' style={divStyle}>EVERY ROUND COUNTS</span>
+                <span className='text-[#A5A7B5] text-lg font-semibold' style={divStyle1}>
+                    Instantly join competitions that suit your style, 
+                    your level, and your schedule with Stryda, your gaming companion. 
+                    It's esports how you want it, when you want it.
+                </span>
+                <div style={divStyle1}>
+                    <ArrowButton />
+                </div>
+            </div>
+            <video className="absolute top-56 z-0 right-0 bottom-0 h-full w-full md:h-auto xl:w-3/4 object-cover opacity-25 lg:opacity-100 mix-blend-lighten" autoplay playsinline loop preload="true" data-cmp-info="9">
+                <source src="https://res.cloudinary.com/gloot/video/upload/v1675240209/Stryda/marketing/website/Stryda_homepage-loop_02.mp4" />
+                <img src="https://res.cloudinary.com/gloot/image/upload/v1676294294/Stryda/marketing/website/Stryda_homepage-loop_02-png.png" alt="" data-cmp-info="9" />
+            </video>
+        </div>
+    )
+}
+
+export default IntroComp;
