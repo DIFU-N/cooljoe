c4e44f968584a29cbeccab7df9beff65eb2afe61
DIFU-N <chukwudifunsofor@gmail.com>
2023-04-13 13:48:41 +0100

    IntroComp-NP regex fix for topicname

--- src/pages/News/IntroComp-N.jsx
+++ src/pages/News/IntroComp-N.jsx
@@ -13,8 +13,8 @@ const IntroComp = () => {
         fontFamily: 'Barlow, sans-serif'
     }
   return (
-    <div className='relative pt-[60px] md:pt-[80px] ml-4 md:ml-0 overflow-x-auto overflow-y-hidden mb-3 bg-[#e1e2e7]'>
-        <div className='flex gap-x-4 py-2'>
+    <div className='relative pt-[60px] md:pt-[80px] md:mx-12 lg:pt-[90px] lg:py-2 ml-4 overflow-x-auto overflow-y-hidden mb-3 bg-[#e1e2e7]'>
+        <div className='flex gap-x-4 py-2 lg:mx-5 lg:py-0'>
         {headlineTopic.map((topic) => (
             // <a href="" key={topic.id} className='active:opacity-50'>
                 <button
@@ -28,6 +28,7 @@ const IntroComp = () => {
                         } else {
                             dispatch(setActiveCategory(topic.tag))
                             dispatch(setBgImage(topic.image));
+                            console.log(topic.name);
                             dispatch(setActiveName(topic.name))
                             dispatch(setActiveText(topic.text))
                         }
@@ -37,8 +38,8 @@ const IntroComp = () => {
                         activeCategory === topic.tag
                           ? "bg-[#101323] text-white"
                           : "bg-white text-[#535669]"
-                      } w-fit h-7 chip-sliced rounded-l-sm py-1 pl-2 pr-3 font-black`}>
-                    {topic.name}
+                      } w-full h-7 chip-sliced rounded-l-sm py-1 pl-2 pr-3 font-black`}>
+                    {topic.name.replace(/ /g, '\u00a0')}
                 </button>
             // </a>
         ))}
