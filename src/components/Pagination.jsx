e2259e2e83f1140e5141d24c758f38eb9738aa76
DIFU-N <chukwudifunsofor@gmail.com>
2023-04-13 15:14:09 +0100

    paginationComp responsive

--- src/components/Pagination.jsx
+++ src/components/Pagination.jsx
@@ -7,7 +7,7 @@ const Pagination = (props) => {
   const [currentItems, setCurrentItems] = useState([]);
   const [pageCount, setPageCount] = useState(0);
   const itemsPerPage = 8;
-  
+
   useEffect(() => {
     const endOffset = itemOffset + itemsPerPage;
     console.log(`Loading items from ${itemOffset} to ${endOffset}`);
@@ -15,26 +15,27 @@ const Pagination = (props) => {
     console.log(currentItems);
     setPageCount(Math.ceil(data.length / itemsPerPage));
   }, [itemOffset, itemsPerPage, data]);
-  
+
   // Invoke when user click to request another page.
   const handlePageClick = (event) => {
     const newOffset = (event.selected * itemsPerPage) % data.length;
     console.log(
       `User requested page number ${event.selected}, which is offset ${newOffset}`
-      );
-      setItemOffset(newOffset);
-    };
-    const divStyle1 = {
-      fontFamily: "Barlow, sans-serif",
-    };
-    
-    return (
-      <>    
-      <div className="grid w-full md:grid-cols-3 md:gap-4 justify-between gap-y-8">
+    );
+    setItemOffset(newOffset);
+    window.scrollTo(0, 0);
+    // const container = document.getElementById("pagination-container");
+    // container.scrollIntoView({ behavior: "smooth" });
+  };
+  const divStyle1 = {
+    fontFamily: "Barlow, sans-serif",
+  };
+
+  return (
+    <>
+      <div id="pagination-container" className="grid w-full md:grid-cols-3 md:gap-4 lg:mb-10 justify-between gap-y-8">
         {currentItems.map((item) => {
-          return(
-            <div>{item}</div>
-          )
+          return <div>{item}</div>;
         })}
       </div>
       <div style={divStyle1}>
@@ -49,8 +50,8 @@ const Pagination = (props) => {
           previousClassName="hidden"
           renderOnZeroPageCount={null}
           activeLinkClassName="bg-[#f3c351] rounded-md flex justify-center items-center w-full h-full text-black"
-          pageClassName="w-[60px] h-[60px] flex justify-center items-center text-[#bbbdc7]"
-          className="list-none flex font-extrabold border-2 rounded-md bg-white justify-center p-6  mx-6 mt-10 items-center mb-[10rem] text-xl gap-2"
+          pageClassName="w-[60px] h-[60px] flex justify-center items-center hover:bg-orange-100 text-[#bbbdc7]"
+          className="list-none flex font-extrabold border-2 rounded-md bg-white justify-center p-6  mx-6 md:mx-0 mt-10 items-center text-xl gap-2"
         />
       </div>
     </>
